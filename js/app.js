ffb35613576d96414d09f7486a383aa655cce23d
SirKentut <pierreharbin@gmail.com>
2022-11-18 20:52:57 -0800

    cleaning again

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
 const btn = document.querySelector(".theme-switch input");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-
+// const theme=localStorage.getItem(document.documentElement);
+// if(theme){
+//   document.documentElement.classList.add(btn);
+// }
 
 
 btn.addEventListener("click", () => {
   if (prefersDarkScheme.matches) {
     document.documentElement.classList.toggle("light_body");
-    return localStorage.setItem(btn,'light_body');
+    localStorage.setItem(theme,'light_body');
     
   } else {
     document.documentElement.classList.toggle("dark");
-    return localStorage.setItem(btn,'dark');
+    localStorage.setItem(theme,'dark');
   }
 });
 
-const theme=localStorage.getItem(btn);
-if(btn){
-    body.classList.add(btn);
-}
 
 
