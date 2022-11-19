7e98d39c202e4ebede24ddd73054c2426cc0c4f3
SirKentut <pierreharbin@gmail.com>
2022-11-18 17:23:35 -0800

    card color

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 const btn = document.querySelector(".theme-switch input");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
+// const theme=localStorage.getItem('theme');
+// if(theme){
+//     body.classList.add(theme);
+// }
+
+
 btn.addEventListener("click", () => {
   if (prefersDarkScheme.matches) {
     document.documentElement.classList.toggle("light_body");
+    // return localStorage.setItem('theme','light_body');
+    
   } else {
     document.documentElement.classList.toggle("dark");
+    // return localStorage.setItem('theme','dark');
   }
 });
+
 
 
