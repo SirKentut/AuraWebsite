48f5f04ecd4bd99d9265a2d5a506c79cca27b6dc
SirKentut <pierreharbin@gmail.com>
2022-11-18 20:04:18 -0800

    Cleaning

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
 const btn = document.querySelector(".theme-switch input");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-// const theme=localStorage.getItem('theme');
-// if(theme){
-//     body.classList.add(theme);
-// }
+
 
 
 btn.addEventListener("click", () => {
   if (prefersDarkScheme.matches) {
     document.documentElement.classList.toggle("light_body");
-    // return localStorage.setItem('theme','light_body');
+    return localStorage.setItem(btn,'light_body');
     
   } else {
     document.documentElement.classList.toggle("dark");
-    // return localStorage.setItem('theme','dark');
+    return localStorage.setItem(btn,'dark');
   }
 });
 
+const theme=localStorage.getItem(btn);
+if(btn){
+    body.classList.add(btn);
+}
 
 
